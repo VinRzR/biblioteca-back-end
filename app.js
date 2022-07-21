b3bc78da7b98c3079fd79b13765c564d79ddc538
Vinicius Rezende <42686498+VinRzR@users.noreply.github.com>
2022-07-21 10:51:35 -0300

    rotas abertas

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 app.use("/", require("./server/routes/associadoRoutes"));
 //teste
-// app.use("/", require("./server/routes/emprestimoRoutes"));
-// app.use("/", require("./server/routes/exemplarRoutes"));
-// app.use("/", require("./server/routes/funcionarioRoutes"));
-// app.use("/", require("./server/routes/publicacaoRoutes"));
-// app.use("/", require("./server/routes/reservaRoutes"));
+app.use("/", require("./server/routes/emprestimoRoutes"));
+app.use("/", require("./server/routes/exemplarRoutes"));
+app.use("/", require("./server/routes/funcionarioRoutes"));
+app.use("/", require("./server/routes/publicacaoRoutes"));
+app.use("/", require("./server/routes/reservaRoutes"));
 
 app.listen(process.env.PORT || 3000);
