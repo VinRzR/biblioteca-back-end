129d83ecc3967e595dc624248386fcfd10904dc0
Vinicius Rezende <42686498+VinRzR@users.noreply.github.com>
2022-07-23 17:20:50 -0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
+var cors = require("cors");
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/", require("./server/routes/associadoRoutes"));
